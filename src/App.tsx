723da45139796fc4a1eafbc5287d21f6df98ab59
Franfercs <ffcs98@hotmail.com>
2024-07-21 16:08:28 -0400

    Change router basename path

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import './index.css';
 
 const App: React.FC = () => {
   return (
-    <Router>
+    <Router basename='/Assessment'>
       <Provider store={store}>
         <Routes>
           <Route path='/' element={<HomePage />} />
